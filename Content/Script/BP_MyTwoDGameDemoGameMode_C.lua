5e1602938ef6ce24be8d05a5e63c77beeb2b6c1c
ZhangZekun <13829062426@163.com>
2021-09-12 10:26:59 +0800

    新增gamemode、savegame、多个level。修改关卡默认gamemode，实现在gamemode内读写savegame。

---
+++ Content/Script/BP_MyTwoDGameDemoGameMode_C.lua
@@ -0,0 +1,37 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+require "UnLua"
+
+local BP_MyTwoDGameDemoGameMode_C = Class()
+
+--function BP_MyTwoDGameDemoGameMode_C:Initialize(Initializer)
+--end
+
+--function BP_MyTwoDGameDemoGameMode_C:UserConstructionScript()
+--end
+
+--function BP_MyTwoDGameDemoGameMode_C:ReceiveBeginPlay()
+--end
+
+--function BP_MyTwoDGameDemoGameMode_C:ReceiveEndPlay()
+--end
+
+-- function BP_MyTwoDGameDemoGameMode_C:ReceiveTick(DeltaSeconds)
+-- end
+
+--function BP_MyTwoDGameDemoGameMode_C:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+--end
+
+--function BP_MyTwoDGameDemoGameMode_C:ReceiveActorBeginOverlap(OtherActor)
+--end
+
+--function BP_MyTwoDGameDemoGameMode_C:ReceiveActorEndOverlap(OtherActor)
+--end
+
+return BP_MyTwoDGameDemoGameMode_C
