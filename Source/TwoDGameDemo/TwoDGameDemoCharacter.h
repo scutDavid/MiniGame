55b6bd3f1f2eb4826614ec1b7f6b6860f4f61979
scutDavid <1902828943@qq.com>
2021-09-15 22:57:31 +0800

    添加轨迹记录和回放2

--- Source/TwoDGameDemo/TwoDGameDemoCharacter.h
+++ Source/TwoDGameDemo/TwoDGameDemoCharacter.h
@@ -52,7 +52,7 @@ protected:
 
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void MyStopJumping();
-
+	UFUNCTION(BlueprintCallable)
 	void UpdateCharacter();
 
 	/** Handle touch inputs. */
