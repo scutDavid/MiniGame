c6415a4fb32a29d7cb6805f5c7134edc26d8bd28
rachelmqxu <rachelmqxu@tencent.com>
2021-09-24 21:30:00 +0800

    角色3C初步版本

---
+++ Plugins/SpinePlugin/Intermediate/Build/Win64/UE4Editor/Inc/SpineEditorPlugin/SpineAtlasImportFactory.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SPINEEDITORPLUGIN_SpineAtlasImportFactory_generated_h
+#error "SpineAtlasImportFactory.generated.h already included, missing '#pragma once' in SpineAtlasImportFactory.h"
+#endif
+#define SPINEEDITORPLUGIN_SpineAtlasImportFactory_generated_h
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_SPARSE_DATA
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_RPC_WRAPPERS
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_RPC_WRAPPERS_NO_PURE_DECLS
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSpineAtlasAssetFactory(); \
+	friend struct Z_Construct_UClass_USpineAtlasAssetFactory_Statics; \
+public: \
+	DECLARE_CLASS(USpineAtlasAssetFactory, UFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SpineEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(USpineAtlasAssetFactory)
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_INCLASS \
+private: \
+	static void StaticRegisterNativesUSpineAtlasAssetFactory(); \
+	friend struct Z_Construct_UClass_USpineAtlasAssetFactory_Statics; \
+public: \
+	DECLARE_CLASS(USpineAtlasAssetFactory, UFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SpineEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(USpineAtlasAssetFactory)
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpineAtlasAssetFactory(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpineAtlasAssetFactory) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpineAtlasAssetFactory); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpineAtlasAssetFactory); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpineAtlasAssetFactory(USpineAtlasAssetFactory&&); \
+	NO_API USpineAtlasAssetFactory(const USpineAtlasAssetFactory&); \
+public:
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpineAtlasAssetFactory(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpineAtlasAssetFactory(USpineAtlasAssetFactory&&); \
+	NO_API USpineAtlasAssetFactory(const USpineAtlasAssetFactory&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpineAtlasAssetFactory); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpineAtlasAssetFactory); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpineAtlasAssetFactory)
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_PRIVATE_PROPERTY_OFFSET
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_38_PROLOG
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_PRIVATE_PROPERTY_OFFSET \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_SPARSE_DATA \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_RPC_WRAPPERS \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_INCLASS \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_PRIVATE_PROPERTY_OFFSET \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_SPARSE_DATA \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_RPC_WRAPPERS_NO_PURE_DECLS \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_INCLASS_NO_PURE_DECLS \
+	MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h_40_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class SpineAtlasAssetFactory."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SPINEEDITORPLUGIN_API UClass* StaticClass<class USpineAtlasAssetFactory>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MiniGame_Plugins_SpinePlugin_Source_SpineEditorPlugin_Public_SpineAtlasImportFactory_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/SpinePlugin/Intermediate/Build/Win64/UE4Editor/Inc/SpinePlugin/SpineSkeletonDataAsset.generated.h
@@ -0,0 +1,116 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SPINEPLUGIN_SpineSkeletonDataAsset_generated_h
+#error "SpineSkeletonDataAsset.generated.h already included, missing '#pragma once' in SpineSkeletonDataAsset.h"
+#endif
+#define SPINEPLUGIN_SpineSkeletonDataAsset_generated_h
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_40_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FSpineAnimationStateMixData_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> SPINEPLUGIN_API UScriptStruct* StaticStruct<struct FSpineAnimationStateMixData>();
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_SPARSE_DATA
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_RPC_WRAPPERS
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_RPC_WRAPPERS_NO_PURE_DECLS
+#if WITH_EDITORONLY_DATA
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ARCHIVESERIALIZER \
+	DECLARE_FSTRUCTUREDARCHIVE_SERIALIZER(USpineSkeletonDataAsset, NO_API)
+
+
+#else
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ARCHIVESERIALIZER
+#endif
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSpineSkeletonDataAsset(); \
+	friend struct Z_Construct_UClass_USpineSkeletonDataAsset_Statics; \
+public: \
+	DECLARE_CLASS(USpineSkeletonDataAsset, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SpinePlugin"), NO_API) \
+	DECLARE_SERIALIZER(USpineSkeletonDataAsset) \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ARCHIVESERIALIZER
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_INCLASS \
+private: \
+	static void StaticRegisterNativesUSpineSkeletonDataAsset(); \
+	friend struct Z_Construct_UClass_USpineSkeletonDataAsset_Statics; \
+public: \
+	DECLARE_CLASS(USpineSkeletonDataAsset, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/SpinePlugin"), NO_API) \
+	DECLARE_SERIALIZER(USpineSkeletonDataAsset) \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ARCHIVESERIALIZER
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpineSkeletonDataAsset(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpineSkeletonDataAsset) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpineSkeletonDataAsset); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpineSkeletonDataAsset); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpineSkeletonDataAsset(USpineSkeletonDataAsset&&); \
+	NO_API USpineSkeletonDataAsset(const USpineSkeletonDataAsset&); \
+public:
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpineSkeletonDataAsset(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpineSkeletonDataAsset(USpineSkeletonDataAsset&&); \
+	NO_API USpineSkeletonDataAsset(const USpineSkeletonDataAsset&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpineSkeletonDataAsset); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpineSkeletonDataAsset); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpineSkeletonDataAsset)
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__rawData() { return STRUCT_OFFSET(USpineSkeletonDataAsset, rawData); } \
+	FORCEINLINE static uint32 __PPO__skeletonDataFileName() { return STRUCT_OFFSET(USpineSkeletonDataAsset, skeletonDataFileName); }
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_53_PROLOG
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_PRIVATE_PROPERTY_OFFSET \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_SPARSE_DATA \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_RPC_WRAPPERS \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_INCLASS \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_PRIVATE_PROPERTY_OFFSET \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_SPARSE_DATA \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_RPC_WRAPPERS_NO_PURE_DECLS \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_INCLASS_NO_PURE_DECLS \
+	MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h_55_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SPINEPLUGIN_API UClass* StaticClass<class USpineSkeletonDataAsset>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MiniGame_Plugins_SpinePlugin_Source_SpinePlugin_Public_SpineSkeletonDataAsset_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
